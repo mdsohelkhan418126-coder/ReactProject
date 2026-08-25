bced12f06a176a78beaddefd0dd4510df149c386
sohelkhan <mdsohelkhan418126@gmail.com>
2026-08-25 15:16:26 +0600

    feat: add team members list component and update App to display members

--- Teammemberlist/src/App.jsx
+++ Teammemberlist/src/App.jsx
@@ -1,9 +1,72 @@
-
-
-
-
+import { Memberlist } from './components/Memberlist';
+const members = [
+  {
+    id: 1,
+    name: 'Alice Johnson',
+    role: 'Frontend Developer',
+    image: 'https://randomuser.me/api/portraits/women/9.jpg',
+  },
+  {
+    id: 2,
+    name: 'Bob Smith',
+    role: 'Backend Developer',
+    image: 'https://randomuser.me/api/portraits/men/20.jpg',
+  },
+  {
+    id: 3,
+    name: 'Charlie Brown',
+    role: 'UI/UX Designer',
+    image: 'https://randomuser.me/api/portraits/women/90.jpg',
+  },
+  {
+    id: 4,
+    name: 'Diana Prince',
+    role: 'Project Manager',
+    image: 'https://randomuser.me/api/portraits/women/71.jpg',
+  },
+  {
+    id: 5,
+    name: 'Ethan Hunt',
+    role: 'DevOps Engineer',
+    image: 'https://randomuser.me/api/portraits/men/77.jpg',
+  },
+  {
+    id: 6,
+    name: 'Mark Gallagher',
+    role: 'QA Engineer',
+    image: 'https://randomuser.me/api/portraits/men/60.jpg',
+  },
+  {
+    id: 7,
+    name: 'George Lucas',
+    role: 'Data Scientist',
+    image: 'https://randomuser.me/api/portraits/men/30.jpg',
+  },
+  {
+    id: 8,
+    name: 'Hannah Baker',
+    role: 'Content Strategist',
+    image: 'https://randomuser.me/api/portraits/women/79.jpg',
+  },
+];
 export default function App() {
   return (
-    <div>App</div>
-  )
+    <div className="min-h-screen p-6 max-w-7xl mx-auto">
+      <h1 className="text-3xl font-bold text-center text-blue-600 mb-6">
+        Team Members List App
+      </h1>
+
+      <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        {/* Profile card */}
+        {members.map(member => (
+          <Memberlist
+            key={member.id}
+            name={member.name}
+            role={member.role}
+            image={member.image}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
---
+++ Teammemberlist/src/components/Memberlist.jsx
@@ -0,0 +1,18 @@
+export const Memberlist = ({ name, role, image }) => {
+  return (
+    <div
+      className=" bg-white shadow-md rounded-lg p-4 border border-gray-200 
+hover:-translate-y-2 transition-all duration-200 ease-in-out cursor-pointer hover:ring-2 ring-green-400 "
+    >
+      <img
+        src={image}
+        alt=""
+        className=" w-32 h-32 rounded-full mx-auto mb-4 object-cover"
+      />
+      <h2 className=" text-xl font-semibold text-gray-800 text-center">
+        {name}
+      </h2>
+      <p className=" text-gray-600 text-center">{role}</p>
+    </div>
+  );
+};
