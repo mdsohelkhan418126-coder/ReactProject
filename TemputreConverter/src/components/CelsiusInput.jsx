3669732adb569cfafe1211d8bf81e32cc99ed7b4
sohelkhan <mdsohelkhan418126@gmail.com>
2026-08-31 11:09:15 +0600

    feat: update CelsiusInput and FahrenheitInput components to accept value and onChange props

--- TemputreConverter/src/components/CelsiusInput.jsx
+++ TemputreConverter/src/components/CelsiusInput.jsx
@@ -1,10 +1,10 @@
 
 
-export const CelsiusInput = () => {
+export const CelsiusInput = ({ value,onChange}) => {
   return (
     <div className=" mb-4">
     <label className=" block font-semibold">Celsius:</label>
-    <input type="number"name="celcsius" id='celsius'placeholder="0 C" className="  rounded p-2 w-full" />
+    <input value={ value} onChange={(e)=>onChange(e.target.value)} type="number"name="celcsius" id='celsius'placeholder="0 C" className="  rounded p-2 w-full" />
     </div>
   )
 }
--- TemputreConverter/src/components/FahrenheitInput.jsx
+++ TemputreConverter/src/components/FahrenheitInput.jsx
@@ -1,10 +1,10 @@
 
 
-export const FahrenheitInput = () => {
+export const FahrenheitInput = ({ value,onChange}) => {
   return (
     <div className=" mb-4">
       <label className=" block font-semibold">Fahrenheit:</label>
-      <input
+      <input value={ value} onChange={(e)=> onChange(e.target.value)}
         type="number"
         name="fahrenheit"
         id="fahrenheit"
